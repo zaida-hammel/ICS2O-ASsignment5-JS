3aa1582631196cf28b87f566b2c25882f800ba22
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-12 00:31:56 +0000

    edited code

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@
 
 /**
  * Check servie worker.
- */
- if (navigator.serviceWorker) {
+*/
+if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS20-Assignment-5-JS/sw.js", {
     scope: "/ICS20-Assignment-5-JS/",
   });
@@ -14,3 +14,26 @@
 /**
  * This function
  */
+function myButtonClicked() {
+  var userInteger = parseInt(document.getElementById("userInteger").value);
+  var pi;
+  var number = 1;
+  var plusOrMinus = "minus";
+
+  pi = 4 / num;
+  while (userInteger > 1) {
+    if (plusOrMinus == "minus") {
+      numbeer = number + 2;
+      pi = pi - (4 / number);
+      plusOrMinus = "plus";
+    } else if (plusOrMinus == "plus") {
+      number = number + 2;
+      pi = pi + (4 / number)
+      plusOrMinus = "minus";
+    }
+    userInteger = userInteger - 1;
+  }
+  plusOrMinus = "minus";
+  number = 1;
+  document.getElementById("answer").innerHTML = "The answer is: " + pi;
+}
